dc1819e8beb94492a05a88df70fc7aa4cbb848de
c8610290927 <c8610290927@gmail.com>
2020-06-19 11:38:10 +0800

    3.1

--- iosFinal/BusRouteRow.swift
+++ iosFinal/BusRouteRow.swift
@@ -11,14 +11,15 @@ import SwiftUI
 struct BusRouteRow: View {
     @ObservedObject var collectData = CollectData()
     var busRoute: BusRoute
-    @State private var isCollect:Int{
+    var isCollect: Int{
         for nowCollect in collectData.collects.indices{
             if collectData.collects[nowCollect].RouteName.Zh_tw == busRoute.RouteName.Zh_tw{
                 return 1
             }
-            return 0
         }
+        return 0
     }
+    @State private var isCollected = 0
     var body: some View {
         HStack{
             VStack(alignment: .leading){
@@ -27,18 +28,35 @@ struct BusRouteRow: View {
             }
             Spacer()
             if isCollect == 0{
-                Image(systemName: "star")
-                .onTapGesture {
-                    self.isCollect = 1
-                    let tmp = BusRoute(RouteName: self.busRoute.RouteName, DepartureStopNameZh: self.busRoute.DepartureStopNameZh,
-                                       DestinationStopNameZh: self.busRoute.DestinationStopNameZh)
-                    self.collectData.collects.insert(tmp, at: 0)
+                if isCollected == 0{
+                    Image(systemName: "star")
+                    .onTapGesture {
+                        self.isCollected = 1
+                        let tmp = BusRoute(RouteName: self.busRoute.RouteName, DepartureStopNameZh: self.busRoute.DepartureStopNameZh,
+                                           DestinationStopNameZh: self.busRoute.DestinationStopNameZh)
+                        self.collectData.collects.insert(tmp, at: 0)
+                    }
+                }else{
+                    Image(systemName: "star.fill")
+                    .onTapGesture {
+                        self.isCollected = 0
+                    }
                 }
             }
             else{
-                Image(systemName: "star.fill")
-                .onTapGesture {
-                    self.isCollect = 0
+                if isCollected == 0{
+                    Image(systemName: "star")
+                    .onTapGesture {
+                        self.isCollected = 1
+                        let tmp = BusRoute(RouteName: self.busRoute.RouteName, DepartureStopNameZh: self.busRoute.DepartureStopNameZh,
+                                           DestinationStopNameZh: self.busRoute.DestinationStopNameZh)
+                        self.collectData.collects.insert(tmp, at: 0)
+                    }
+                }else{
+                    Image(systemName: "star.fill")
+                    .onTapGesture {
+                        self.isCollected = 0
+                    }
                 }
             }
         }
